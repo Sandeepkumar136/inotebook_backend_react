02817bd1366985838e97973281da8aed25a46864
Sandeepkumar136 <ksundip750@gmail.com>
2024-08-01 00:10:39 +0530

    HALF COMPLITED VALIDATION

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,29 @@
-const express=require('express');
-const User=require('../models/User');
-const router=express.Router();
-
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 // Create a user using POST: '/api/auth' Doesnt require auth
-router.post('/', (req,res)=>{
-    console.log(req.body);
-    const user=User(req.body);
-    user.save()
-    res.send(req.body);
-});
+router.post(
+  "/",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password
+    }).then(user=>res.json(user))
+  }
+);
 
-module.exports=router
+module.exports = router;
