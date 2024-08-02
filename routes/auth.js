fdc8f6ae9bfbda90996c9e314d4d83b2dbfbc7ff
Sandeepkumar136 <ksundip750@gmail.com>
2024-08-02 13:23:40 +0530

    Configured JsonWebToken

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "SandeepKumar@136";
 
 // Create a user using POST: '/api/auth' Doesnt require auth
 router.post(
@@ -12,7 +16,8 @@ router.post(
     body("password", "Password must be atleast 5 characters").isLength({
       min: 5,
     }),
-  ],async (req, res) => {
+  ],
+  async (req, res) => {
     // if there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -20,23 +25,35 @@ router.post(
     }
     // Check whether the user with this email exists already
     try {
-      let user=await User.findOne({email:req.body.email});
-      if(user){
-        return res.status(400).json({error:"Sorry a user with this email already exists"})
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
       }
-      user =await User.create({
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      // Create a new user
+      user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
-      })
-      res.json(user)
+        password: secPass,
+      });
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authtoken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ authtoken });
     } catch (error) {
       console.error(error.message);
-      res.status(500).send('Some Error Occoured')
+      res.status(500).send("Some Error Occoured");
     }
-
-
-
   }
 );
 
