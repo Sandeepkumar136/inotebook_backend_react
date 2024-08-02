0694e9d2f2e9d0774c00edff2e6e8d53c9fb8f02
Sandeepkumar136 <ksundip750@gmail.com>
2024-08-02 12:53:07 +0530

    Managing Requests

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const connectToMongo=require('./db');
 connectToMongo();
 
-const app = express()
-const port = 5000
-app.use(express.json())
+const app = express();
+const port = 5000;
+app.use(express.json());
 
 // Available Routes
 app.use('/api/auth', require('./routes/auth'));
--- routes/auth.js
+++ routes/auth.js
@@ -5,34 +5,38 @@ const { body, validationResult } = require("express-validator");
 
 // Create a user using POST: '/api/auth' Doesnt require auth
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("email", "Enter a valid Email").isEmail(),
     body("password", "Password must be atleast 5 characters").isLength({
       min: 5,
     }),
-  ],
-  (req, res) => {
+  ],async (req, res) => {
+    // if there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log("Error creating user:",err);
-        if(err.code===11000){
-          return res.status(400).json({
-            error:'Please enter a unique value for email',
-            message:"Email already exists"
-          });
-        }
-      });
+    // Check whether the user with this email exists already
+    try {
+      let user=await User.findOne({email:req.body.email});
+      if(user){
+        return res.status(400).json({error:"Sorry a user with this email already exists"})
+      }
+      user =await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      })
+      res.json(user)
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Some Error Occoured')
+    }
+
+
+
   }
 );
 
